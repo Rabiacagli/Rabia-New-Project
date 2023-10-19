243aab19a1d168d9630035bd138092e80f89929d
Rabiacagli <rabiaacgl@gmail.com>
2023-10-19 18:24:32 +0300

    commit

--- src/VkiHesaplama.java
+++ src/VkiHesaplama.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class VkiHesaplama {
+import java.util.Scanner;
+public class VkiHesaplama {
+    public static void main(String[] args) {
+    Scanner input= new Scanner(System.in);
+    double boy, kilo, vki ;
+        System.out.print("Boyunuzu Giriniz: ");
+    boy=input.nextDouble();
+    System.out.print("Kilonuzu Giriniz: ");
+    kilo=input.nextDouble();
+    vki=kilo/(boy*boy);
+
+        System.out.println("Vücut Kitle İndeksiniz: " + vki);
+
+    }
 }
