12ee45e4b456ffac635b2269d10fa36c04fa1d44
Rabiacagli <rabiaacgl@gmail.com>
2023-11-02 11:22:20 +0300

    Ortalama Alma

--- src/UcVeDordeBolunebilenSayilar.java
+++ src/UcVeDordeBolunebilenSayilar.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 public class UcVeDordeBolunebilenSayilar {
     public static void main(String[] args) {
-
+        int i = 0;
         int r;
+        int toplam = 0 ;
+
+
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Sayı Giriniz: ");
-        r = input.nextInt();
 
-        for ( int i = 0 ; i <= r ; i++ ){
-            if ( i % 3 == 0 && i % 4 == 0){
-
-                    System.out.print(i + ",");
-                }
+        do {
+            System.out.print("Sayı Giriniz: ");
+            r = input.nextInt();
+            if (r % 3 == 0 && r % 4 == 0){
+                toplam += r;
+                i++ ;
             }
 
-        }
+        } while (r>0);
 
+
+        System.out.println( "Toplam: " + toplam/i );
     }
+}
+
+
+
+
 
 
 
